434592b2e10ac9ce9fd371cf615f2aa5b0d36ced
minhhuy <levuminhhuy99@gmail.com>
2020-08-30 01:49:23 +0700

    hi

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { CrudService } from './services/crud.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://45.119.81.84:5000/ocr',
+  firebaseConfig: {
+    apiKey: "AIzaSyA1DQpPRfs9XxFc1RAOrRXY2Dn7o_iTiIQ",
+    authDomain: "fir-ocr-ec83a.firebaseapp.com",
+    databaseURL: "https://fir-ocr-ec83a.firebaseio.com",
+    projectId: "fir-ocr-ec83a",
+    storageBucket: "fir-ocr-ec83a.appspot.com",
+    messagingSenderId: "486449689562",
+    appId: "1:486449689562:web:3d4ce49e881c07d52706e3"
+  },
+  // googleAPI: 'AIzaSyC1tn2sacBwFc-Q3TyxilRs2-IBQc4N8i8'
+  mapboxAPI: 'pk.eyJ1IjoibWhobW0iLCJhIjoiY2tlZWpqejNqMTM4OTJ0cDdldHI2MTk4OSJ9.JSuP3IsBnKayJa7Et96_DQ'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
